448a6601740556f43dd5a5dea8749cfce119eb47
César González <chesarglez429@gmail.com>
2021-06-12 13:26:59 -0500

    :recycle: Extract function that replaces blob area

--- Assets/Scripts/MoveBlobs.cs
+++ Assets/Scripts/MoveBlobs.cs
@@ -7,35 +7,28 @@ public static class Helpers
 {
     // -- The juicy stuff --
 
-    public static float GrowBlob(GameObject blob, float eatedArea)
-    {
-        var blobArea = GetBlobArea(blob);
-        var newArea = blobArea + eatedArea;
-
-        var diameter = GetDiameter(newArea);
-        var difference = Mathf.Abs(diameter - blob.transform.localScale.x);
-
-        blob.transform.localScale = new Vector3(diameter, diameter, DEFAULT_Z);
-
-        return difference;
-    }
-
-    public static float ShrinkBlob(GameObject blob, float shrinkedArea)
-    {
-        var blobArea = GetBlobArea(blob);
-        var newArea = blobArea - shrinkedArea;
-
-        if (newArea < MIN_BLOB_AREA)
+    public static Func<float, Func<GameObject, float>>
+        GrowBlob = eatedArea => blob =>
         {
-            return 0;
-        }
+            var blobArea = GetBlobArea(blob);
+            var newArea = blobArea + eatedArea;
 
-        var diameter = GetDiameter(newArea);
-        var difference = Mathf.Abs(diameter - blob.transform.localScale.x);
+            return ReplaceBlobArea(newArea)(blob);
+        };
 
-        blob.transform.localScale = new Vector3(diameter, diameter, DEFAULT_Z);
-        return difference;
-    }
+    public static Func<float, Func<GameObject, float>>
+        ShrinkBlob = shrinkedArea => blob =>
+        {
+            var blobArea = GetBlobArea(blob);
+            var newArea = blobArea - shrinkedArea;
+
+            if (newArea < MIN_BLOB_AREA)
+            {
+                return 0;
+            }
+
+            return ReplaceBlobArea(newArea)(blob);
+        };
 
     public static readonly Func<GameObject, Func<int, List<GameObject>>>
         CreatePool = gameObject => amount =>
@@ -70,5 +63,16 @@ public static class Helpers
             var instance = UnityEngine.Object.Instantiate(gameObject);
             instance.SetActive(false);
             return instance;
+        };
+
+    private static readonly Func<float, Func<GameObject, float>>
+        ReplaceBlobArea = newArea => blob =>
+        {
+            var newDiameter = GetDiameter(newArea);
+            var currentDiameter = blob.transform.localScale.x;
+            blob.transform.localScale = new Vector3(newDiameter, newDiameter, DEFAULT_Z);
+            
+            var diameterDifference = currentDiameter - newDiameter;
+            return diameterDifference;
         };
 }
