89b19e1465fb25fac5d2528984a0b13bca02834b
RafaelCantoVazquez <rafacantovaz@gmail.com>
2021-06-12 21:16:36 -0500

    :sparkles: Implement change controling blob

--- Assets/Scripts/BlobsManager.cs
+++ Assets/Scripts/BlobsManager.cs
@@ -7,25 +7,24 @@ using UnityEngine;
 public class BlobsManager : MonoBehaviour
 {
     public static BlobsManager Instance;
-    private bool _isReady = false;
     public bool IsReady { get; private set; }
     public GameObject objectToPool;
     public int amountToPool = 20;
     private PlayerPlatformerController _currentBlob;
     private List<GameObject> pool;
+    private int activeBlobsIndex;
 
-    private void Awake() => Instance = this;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
+        activeBlobsIndex = 0;
+        Instance = this;
         pool = Helpers.CreatePool(objectToPool)(amountToPool);
         foreach (var obj in pool)
         {
             obj.transform.parent = transform;
         }
         IsReady = true;
-    }
+    } 
 
     public GameObject GetObject()
     {
@@ -47,8 +46,54 @@ public class BlobsManager : MonoBehaviour
 
         Helpers.HandleHorizontalMovement(_currentBlob, _currentBlob.spriteRenderer, 5);
 
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            bool blobFound = false;
+            
+            for (int i = activeBlobsIndex; i < pool.Count; i++)
+            {
+                if (CanBeSelected(pool[i]))
+                {
+                    // Deselect current blob
+                    _currentBlob.targetVelocity = Vector2.zero;
+                    _currentBlob.GetComponent<SpriteRenderer>().color = Color.white;
+                    
+                    // Select new blob
+                    _currentBlob = pool[i].GetComponent<PlayerPlatformerController>();
+                    _currentBlob.GetComponent<SpriteRenderer>().color = Color.cyan;
+                    activeBlobsIndex = i;
+                    blobFound = true;
+                    break;
+                }
+            }
+
+            if (!blobFound)
+            {
+                for (int i = 0; i < activeBlobsIndex; i++)
+                {
+                    if (!pool[i].GetComponent<PlayerPlatformerController>().Equals(_currentBlob) && pool[i].activeSelf)
+                    { 
+                        _currentBlob.targetVelocity = Vector2.zero;
+                        _currentBlob.GetComponent<SpriteRenderer>().color = Color.white;
+                        _currentBlob = pool[i].GetComponent<PlayerPlatformerController>();
+                        _currentBlob.GetComponent<SpriteRenderer>().color = Color.cyan;
+                        activeBlobsIndex = i;
+                        blobFound = true;
+                        break;
+                    }
+                }
+            }
+        }
     }
 
-    public void ChangeCurrentBlob(PlayerPlatformerController blob) => _currentBlob = blob;
-    
+    private bool CanBeSelected(GameObject blob)
+    {
+        return !blob.GetComponent<PlayerPlatformerController>().Equals(_currentBlob) && blob.activeSelf;
+    }
+
+    public void ChangeCurrentBlob(PlayerPlatformerController blob)
+    {
+        _currentBlob = blob;
+        _currentBlob.GetComponent<SpriteRenderer>().color = Color.cyan;
+    }
 }
